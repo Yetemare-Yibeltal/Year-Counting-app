8f8a34747fc5f8b68b6200e768a10a351f8d1bc5
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-22 15:50:22 +0300

    add time units and calendar constants

---
+++ server/src/constants/time.constants.ts
@@ -0,0 +1,37 @@
+export const TIME_CONSTANTS = {
+  MS_PER_SECOND: 1000,
+  SECONDS_PER_MINUTE: 60,
+  MINUTES_PER_HOUR: 60,
+  HOURS_PER_DAY: 24,
+  DAYS_PER_COMMON_YEAR: 365,
+  DAYS_PER_LEAP_YEAR: 366,
+  MONTHS_PER_YEAR: 12,
+  QUARTERS_PER_YEAR: 4,
+  MONTHS_PER_QUARTER: 3,
+  DEFAULT_TIMEZONE: "UTC",
+};
+
+export const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const DAY_NAMES = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
