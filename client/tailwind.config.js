b365484c8a63f06edf8106daace14b28ee955433
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 04:37:18 +0300

    config: update template paths for tailwind scanner

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,15 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        dark: {
-          900: "#09090b",
-          800: "#121215",
-          700: "#1c1c21",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
