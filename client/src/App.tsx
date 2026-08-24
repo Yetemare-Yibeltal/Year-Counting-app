61f8a4c76ebd2b02f1ca93a70bc39c2bb943a057
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 04:44:44 +0300

    refactor: remove auth provider and route protection from App router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-
-// Import pages/components
 import { Home } from "./pages/Home";
 
 export const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Add additional public & protected routes here */}
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
