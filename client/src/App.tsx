bf96c311820fddd82ac0da6086372be14c7b9f43
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 04:41:53 +0300

    fix: change Home import in App.tsx to named import syntax

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
+// Import pages/components
 import { Home } from "./pages/Home";
 
 export const App: React.FC = () => {
@@ -9,6 +12,7 @@ export const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* Add additional public & protected routes here */}
         </Routes>
       </BrowserRouter>
     </AuthProvider>
