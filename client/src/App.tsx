3b71f5b60d1804bff891cdd7d1e4286a3b9c1c02
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 04:03:37 +0300

    register Unauthorized 403 route in App router config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
-// Pages (adjust imports based on your page names)
+// Pages
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Dashboard } from "./pages/Dashboard";
 import { AdminPanel } from "./pages/AdminPanel";
+import { Unauthorized } from "./pages/Unauthorized";
 
 const App: React.FC = () => {
   return (
@@ -17,6 +18,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
 
         {/* Authenticated User Routes */}
         <Route element={<ProtectedRoute />}>
