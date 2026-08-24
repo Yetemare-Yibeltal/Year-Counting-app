c77a5d38eb71a7ea5f90de125cb394088f895b99
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 04:41:16 +0300

    fix: update Home import to named export in App router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-
-// Import your pages/components
-import Home from "./pages/Home";
+import { Home } from "./pages/Home";
 
 export const App: React.FC = () => {
   return (
@@ -12,7 +9,6 @@ export const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Add additional public & protected routes here */}
         </Routes>
       </BrowserRouter>
     </AuthProvider>
