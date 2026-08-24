2bbd01a3c7819111f2700e266104144dfbe05e31
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-24 05:19:12 +0300

    update user routes to import correct auth middleware and cache string

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -6,6 +6,6 @@ import { cacheMiddleware } from "../middleware/cache";
 const router = Router();
 
 router.route("/")
-  .get(authenticate, cacheMiddleware("300s"), getUsers);
+  .get(authenticate, cacheMiddleware("300s"), getUsers3);
 
 export default router;
