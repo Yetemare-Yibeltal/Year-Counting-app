4832f6b7735520b827edd263edadd0ef58d8111d
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-25 13:56:13 +0300

    Fix redisClient alias export in cacheMiddleware bridge

--- server/src/middleware/cacheMiddleware.ts
+++ server/src/middleware/cacheMiddleware.ts
@@ -1,23 +1,3 @@
-import { Request, Response, NextFunction } from 'express';
-import { redisClient } from '../config/redis';
-
-export const cacheMiddleware = (duration: string) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    if (redisClient.status !== 'ready') {
-      return next();
-    }
-
-    const key = `cache:${req.originalUrl}`;
-    try {
-      const cachedData = await redisClient.get(key);
-      if (cachedData) {
-        res.setHeader('X-Cache', 'HIT');
-        res.json(JSON.parse(cachedData));
-        return;
-      }
-    } catch {
-      return next();
-    }
-    next();
-  };
-};
+import { redis as redisClient } from "../lib/redis";
+export { redisClient };
+export { cacheMiddleware } from "./cache";
