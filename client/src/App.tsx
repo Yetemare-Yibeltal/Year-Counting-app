494c739be123ae4690370566d63c797b023ad1a6
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-25 14:48:08 +0300

    refactor(router): mount enhanced Home component as default root route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/layout/Header";
-import { LiveTracker } from "./components/features/LiveTracker";
-import { CalculatorSuite } from "./components/features/CalculatorSuite";
-import { SystemSpecs } from "./components/features/SystemSpecs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-950 text-gray-100 font-sans">
-      <Header />
-      <main className="max-w-6xl mx-auto py-8">
-        <Routes>
-          <Route path="/" element={<LiveTracker />} />
-          <Route path="/calculator" element={<CalculatorSuite />} />
-          <Route path="/specs" element={<SystemSpecs />} />
-          <Route path="*" element={<LiveTracker />} />
-        </Routes>
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
