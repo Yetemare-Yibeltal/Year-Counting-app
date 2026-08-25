829ac515be2705c87907b5112c96c34e5d3b79f4
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-25 14:48:25 +0300

    mount enhanced Home component as default root route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import { LiveTracker } from "./components/features/LiveTracker";
+import { CalculatorSuite } from "./components/features/CalculatorSuite";
+import { SystemSpecs } from "./components/features/SystemSpecs";
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="min-h-screen bg-gray-950 text-gray-100 font-sans">
+      <Header />
+      <main className="max-w-6xl mx-auto py-8">
+        <Routes>
+          <Route path="/" element={<LiveTracker />} />
+          <Route path="/calculator" element={<CalculatorSuite />} />
+          <Route path="/specs" element={<SystemSpecs />} />
+          <Route path="*" element={<LiveTracker />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
